e0a6256e5ff1d72cce748b42d34ba9cf1214e266
Yaypixel <thurstonreese09@outlook.com>
2025-05-24 17:31:06 -0400

    Initial Commit

---
+++ HackathonFinalProject/HackathonFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonFinalProjectApp.swift
+//  HackathonFinalProject
+//
+//  Created by Thurston Reese on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct HackathonFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
